4a9331632f009a42b1f0f497e85487f2918110f7
Mustafa Khan <mustafakhan98@gmail.com>
2021-02-28 18:52:52 +0300

    Button full functionality

--- html-css/index.js
+++ html-css/index.js
@@ -2,10 +2,26 @@ const btn = document.querySelector('.button')
 // const top = document.getElementsByClassName('.devmtn')
 
 btn.addEventListener('click', function(){
+    
+    if(btn.innerText === 'View Larger'){
+
+    btn.innerText = 'View Smaller'
+
     const top = document.querySelectorAll('.devmtn')
     for(i = 0; i <= 6; i++){
         top[i].classList.add('new-devmtn')
         top[i].classList.remove('devmtn')
 
+        }
+    }else{
+        btn.innerText = 'View Larger'
+
+        const top = document.querySelectorAll('.new-devmtn')
+        for(i = 0; i <= 6; i++){
+
+        top[i].classList.add('devmtn')
+        top[i].classList.remove('new-devmtn')
+
+        }
     }
 })
